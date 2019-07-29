486c84c2aee9efa1ae8a675c14fb4f350ab82a3a
tom <gaoqiang19514@163.com>
2019-07-29 21:41:37 +0800

    优化代码，添加bilibili的按钮状态检测

--- src/js/page.js
+++ src/js/page.js
@@ -3,16 +3,16 @@ function notHaveClass($elem, className) {
   return !$elem.hasClass(className);
 }
 
-function closeDanmuBtn(selector) {
+function closeDanmuBtn(selector, notDisabledStatusClassFunc) {
   var $closeBtn = $(selector);
   count += 1;
   console.log("尝试查找弹幕关闭按钮。。。", $closeBtn);
-  if ($closeBtn.length && notHaveClass($closeBtn, "danmu-hide-btn")) {
+  if ($closeBtn.length && notDisabledStatusClassFunc($closeBtn)) {
     $closeBtn.trigger("click");
     console.log("关闭弹幕成功");
   } else if (count < TRY_COUNT) {
     setTimeout(() => {
-      closeDanmuBtn(selector);
+      closeDanmuBtn(selector, notDisabledStatusClassFunc);
     }, 500);
   }
 }
@@ -20,8 +20,28 @@ function closeDanmuBtn(selector) {
 var TRY_COUNT = 100;
 var count = 0;
 $(function() {
-  // 关闭虎牙弹幕按钮
-  closeDanmuBtn("#player-danmu-btn");
-  // 关闭B站弹幕
-  closeDanmuBtn(".bilibili-player-video-danmaku-switch .bui-checkbox");
+
+  if (window.location.host.indexOf('www.huya.com') !== -1) {
+    console.log('huya')
+    // 关闭虎牙弹幕按钮
+    closeDanmuBtn("#player-danmu-btn", function($elem) {
+      if ($elem.hasClass("danmu-hide-btn")) {
+        return false
+      }
+      return true
+    });
+  }
+    
+  if (window.location.host.indexOf('www.bilibili.com') !== -1) {
+    console.log('bilibili')
+    // 关闭B站弹幕
+    closeDanmuBtn(".bilibili-player-video-danmaku-switch .bui-checkbox", function() {
+      if (!$(".bilibili-player-video-danmaku-switch.bui.bui-switch .bui-checkbox").attr('checked')) {
+        return false
+      }
+      return true
+    });
+  }
+
 });
+ 
