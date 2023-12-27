da51c3becfd22b8617a64813426ee62da7178657
tomcat <gaoqiang19514@163.com>
2023-12-27 15:33:38 +0800

    feat: 完成自动计算和添加剪贴板功能

--- js/content.js
+++ js/content.js
@@ -30,8 +30,9 @@ $(function () {
     const arr = text.match(/[a-zA-Z]+|\d+/g);
     const [key1, key1Value, , key2, key2Value, key2Target] = arr;
 
-    const newValue = `${key1} ${key1Value + 1} <br /> ${key2} (${key2Value + 2}/${key2Target})`
-    console.log('newValue', newValue)
+    const newValue = `${key1} ${Number(key1Value) + 1}\n${key2} (${Number(key2Value) + 2}/${key2Target})`
     // TODO: 如何将内容写入到textarea中？
+    // 获取不到vue的实例，那就换一个思路，将内容写入剪贴板
+    navigator.clipboard.writeText(newValue)
   });
 });
