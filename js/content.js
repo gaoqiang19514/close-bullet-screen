18e07fa22b49c2b4af190cd9abafb7312d97185f
refanbanzhang <gaoqiang19514@163.com>
2024-03-13 20:40:43 +0800

    feat: 添加自动写入textarea的功能

--- js/content.js
+++ js/content.js
@@ -1,22 +1,45 @@
-$(function () {
-  $('.woo-pop-wrap-main button').click(() => {
-    const punchInPosts = $('.vue-recycle-scroller__item-wrapper .vue-recycle-scroller__item-view')
-      .filter((_, item) => {
-        const text = $(item).find(':first .wbpro-feed-ogText div').html()
-        const arr = text.match(/[a-zA-Z]+|\d+/g);
-        const [key1, key1Value, br, key2] = arr;
-
-        return key1 === 'f' && br === 'br' && key2 === 'dw'
-      })
-
-    const [firstPunchInPost] = punchInPosts;
-    const text = $(firstPunchInPost).find(':first .wbpro-feed-ogText div').html()
+/**
+ * 获取最新一条打卡文本内容
+ * @returns {string}
+ */
+const getNewestPost = () => {
+  const punchInPosts = $(
+    ".vue-recycle-scroller__item-wrapper .vue-recycle-scroller__item-view"
+  ).filter((_, item) => {
+    const text = $(item).find(":first .wbpro-feed-ogText div").html();
     const arr = text.match(/[a-zA-Z]+|\d+/g);
-    const [key1, key1Value, _, key2, key2Value, key2Target] = arr;
+    const [key1, key1Value, br, key2] = arr;
 
-    const newValue = `${key1} ${Number(key1Value) + 1}\n${key2} (${Number(key2Value) + 2}/${key2Target})`
-    // TODO: 如何将内容写入到textarea中？
-    // 获取不到vue的实例，那就换一个思路，将内容写入剪贴板
-    navigator.clipboard.writeText(newValue)
+    return key1 === "f" && br === "br" && key2 === "dw";
+  });
+
+  const [firstPunchInPost] = punchInPosts;
+  const text = $(firstPunchInPost).find(":first .wbpro-feed-ogText div").html();
+  const arr = text.match(/[a-zA-Z]+|\d+/g);
+  const [key1, key1Value, _, key2, key2Value, key2Target] = arr;
+
+  return `${key1} ${Number(key1Value) + 1}\n${key2} (${
+    Number(key2Value) + 2
+  }/${key2Target})`;
+};
+
+/**
+ * 将文本插入textare中
+ * @param {string} text
+ */
+const insertTextarea = (text) => {
+  if (!text) {
+    throw new Error("写入值不能为空");
+  }
+  const textarea = document.querySelector("textarea");
+  const inputEvent = new Event("input", { bubbles: true });
+
+  textarea.value = text;
+  textarea.dispatchEvent(inputEvent);
+};
+
+$(function () {
+  $(".woo-pop-wrap-main button").click(() => {
+    insertTextarea(getNewestPost());
   });
 });
