7a989ae4b2378a841fd4377bebc1b2ab2d3d18f9
refanbanzhang <gaoqiang19514@163.com>
2024-03-13 12:22:04 +0800

    feat: 添加非打卡内容的复制过滤

--- js/content.js
+++ js/content.js
@@ -1,8 +1,18 @@
 $(function () {
   $('.woo-pop-wrap-main button').click(() => {
-    const text = $('.vue-recycle-scroller__item-wrapper .vue-recycle-scroller__item-view:first .wbpro-feed-ogText div').html();
+    const punchInPosts = $('.vue-recycle-scroller__item-wrapper .vue-recycle-scroller__item-view')
+      .filter((_, item) => {
+        const text = $(item).find(':first .wbpro-feed-ogText div').html()
+        const arr = text.match(/[a-zA-Z]+|\d+/g);
+        const [key1, key1Value, br, key2] = arr;
+
+        return key1 === 'f' && br === 'br' && key2 === 'dw'
+      })
+
+    const [firstPunchInPost] = punchInPosts;
+    const text = $(firstPunchInPost).find(':first .wbpro-feed-ogText div').html()
     const arr = text.match(/[a-zA-Z]+|\d+/g);
-    const [key1, key1Value, , key2, key2Value, key2Target] = arr;
+    const [key1, key1Value, _, key2, key2Value, key2Target] = arr;
 
     const newValue = `${key1} ${Number(key1Value) + 1}\n${key2} (${Number(key2Value) + 2}/${key2Target})`
     // TODO: 如何将内容写入到textarea中？
